33d4f8919ecc67654b98ea11a8ee0c3bf9e6c79d
timspurgion <timspurgion@gmail.com>
2024-12-13 00:36:02 +0100

    Javascript functionality added

---
+++ orders.js
@@ -0,0 +1,38 @@
+const order = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'DJI Ronin S drone',
+        productNumber: '48931',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Canon EOS 70D',
+        productNumber: '80941',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Crane Gimbal 460D',
+        productNumber: '48951',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Iphone 16 pro max',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Samsung 64 inches oled screen',
+        productNumber: '04837',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
